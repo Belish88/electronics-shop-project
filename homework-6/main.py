f70722d49ed565f5f4577501b3ec150769f00268
Belish88 <belish888@rambler.ru>
2023-08-14 17:30:15 +0300

    modified main.py: added path from setting.py

--- homework-6/main.py
+++ homework-6/main.py
@@ -1,10 +1,11 @@
 from src.item import Item
+from src.setting import CSVTEST, NOTFILE
 
 if __name__ == '__main__':
     # Файл items.csv отсутствует.
-    Item.instantiate_from_csv()
+    Item.instantiate_from_csv(NOTFILE)
     # FileNotFoundError: Отсутствует файл item.csv
 
     # В файле items.csv удалена последняя колонка.
-    Item.instantiate_from_csv()
+    Item.instantiate_from_csv(CSVTEST)
     # InstantiateCSVError: Файл item.csv поврежден
